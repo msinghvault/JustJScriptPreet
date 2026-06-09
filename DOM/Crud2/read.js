2ff44c17c714e06a7128f4199732b69cc9f861b3
Manpreet Singh <21085737+msinghvault@users.noreply.github.com>
2026-06-09 14:53:14 -0500

    CrudOperations

---
+++ DOM/Crud2/read.js
@@ -0,0 +1,16 @@
+
+function displayUsers() {
+    document.querySelector("tbody").innerHTML = "";
+    allUsers.forEach(function(usr,index){
+        var myTr = document.createElement('tr');
+        myTr.innerHTML = `
+        <td>${usr.fname}</td>
+        <td>${usr.lname}</td>
+        <td>${usr.email}</td>
+        <td><button onclick="editUser(${index})">Edit</button></td>
+        <td><button onclick="deleteUser(${index})">Delete</button></td>`;
+
+        document.querySelector("tbody").appendChild(myTr)
+    })
+}
+displayUsers();
